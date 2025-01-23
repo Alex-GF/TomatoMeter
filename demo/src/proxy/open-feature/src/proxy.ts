a8899c3bd92ac017912ca53a438a3747702b3e07
Alex-GF <yalejandro9@gmail.com>
2025-01-23 14:16:21 +0100

    fix: missed changes in previous commit

--- demo/src/proxy/open-feature/src/proxy.ts
+++ demo/src/proxy/open-feature/src/proxy.ts
@@ -1,18 +1,23 @@
-import {
-  OpenFeature,
-  useBooleanFlagValue,
-  useFlag,
-} from '@openfeature/react-sdk';
+import { OpenFeature, useBooleanFlagValue, useFlag } from '@openfeature/react-sdk';
 import { PricingDrivenFeaturesProvider } from '../provider/react/PricingDrivenFeaturesReact';
 
 export function initializeOpenFeature() {
-  OpenFeature.setProvider(new PricingDrivenFeaturesProvider("http://sphere.score.us.es/static/pricings/uploadedDataset/Pricing-driven%20Feature%20Toggling%20Demo/2025-1-8.yml"), {"test": "1"});
+  OpenFeature.setProvider(new PricingDrivenFeaturesProvider(), {
+    pricingUrl:
+      'http://sphere.score.us.es/static/pricings/uploadedDataset/Pricing-driven%20Feature%20Toggling%20Demo/2025-1-8.yml',
+    userContext: {
+      user: 'test',
+      maxExpenses: 2,
+    },
+  });
 
   return {
     getFeature: (key: string) => {
       console.log('getFeature', key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase());
-      console.log(useBooleanFlagValue(key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(), false))
-      console.log(useFlag(key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(), false))
+      console.log(
+        useBooleanFlagValue(key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(), false)
+      );
+      console.log(useFlag(key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(), false));
       return useBooleanFlagValue(key, false);
     },
   };
