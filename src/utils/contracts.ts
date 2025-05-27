aab73d027c3802bd5e5a09483121247a04ea2bf9
Alex-GF <yalejandro9@gmail.com>
2025-05-28 00:01:09 +0200

    fix: handling of add-ons

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -15,8 +15,9 @@ export async function updateContract(plan: string, addons: Record<string, number
   });
 }
 
-export const toSubscriptionArr = (plan: string, addons: { [key: string]: number }) => {
+export const toSubscriptionArr = (plan: string, addons?: { [key: string]: number }) => {
     const arr = [plan];
+    if (!addons) return arr;
     Object.entries(addons).forEach(([name, v]) => {
       if (v > 0) arr.push(`${name}X${v}`);
     });
